fe6339f898b764b6c260e220f318c1883a39672b
QuangLink <100298893+QuangLink@users.noreply.github.com>
2023-10-21 01:36:01 +0700

    add cors origin

--- app.js
+++ app.js
@@ -1,30 +1,25 @@
 const express = require('express');
-const cors = require('cors');
+const session = require('express-session');
 const createError = require('http-errors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
 const app = express();
 
-// Sử dụng cors middleware với các tùy chọn cần thiết
-app.use(cors({
-  origin: 'http://localhost:3000', // Thay đổi thành địa chỉ nguồn của bạn
-  credentials: true,
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: 'Content-Type, Authorization, Origin, Accept',
-}));
+app.use(function(req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = ['http://localhost:3000', ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
 
-// Các cài đặt khác của ứng dụng Express
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Routes của bạn
+// Importing routes
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const productRouter = require('./routes/products');
@@ -32,19 +27,27 @@ const adminRouter = require('./routes/admin');
 const categoryRouter = require('./routes/category');
 const cartRouter = require('./routes/cart');
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+// Routes
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productRouter);
 app.use('/admin', adminRouter);
 app.use('/category', categoryRouter);
 app.use('/cart', cartRouter);
-
-// Middleware xử lý lỗi 404 Not Found
+// 404 Not Found middleware
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// Middleware xử lý lỗi
+// Error handler
 app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
