ed9c7b18db8ff1b6e499a105564209b4e5867095
QuangLink <100298893+QuangLink@users.noreply.github.com>
2023-10-21 00:51:27 +0700

    add cors rule

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const db = require('./../models/database');
-
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", 
+  "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
 router.get('/', (req, res) => {
   const query = 'SELECT * FROM product';
   db.query(query, (error, results) => {
