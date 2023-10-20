afca25da3ef6a52e7ac28719e2a424594421807b
QuangLink <100298893+QuangLink@users.noreply.github.com>
2023-10-20 18:23:08 +0700

    update

--- app.js
+++ app.js
@@ -32,7 +32,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  // Các tùy chọn CORS khác nếu cần
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
