b842c8f8fe28c986aebb13d0b4d0aadf0e06dc93
QuangLink <100298893+QuangLink@users.noreply.github.com>
2023-10-20 17:53:58 +0700

    update env, config connection dtb, remove nodemon

--- models/database.js
+++ models/database.js
@@ -1,9 +1,10 @@
 var mysql = require('mysql');
+require('dotenv').config();
 var db = mysql.createConnection({
-   host: 'localhost', 
-   user: 'root', 
-   password: '', 
-   database: 'duantn'
+   host: process.env.HOST,
+   user: process.env.USER, 
+   password: process.env.PASSWORD, 
+   database: process.env.DATABASE,
 }); 
 db.connect(() => console.log('Da ket noi database !'));
 module.exports = db; 
