03b5aaf4798fcc2d0da9e6519d37d65e127cf63a
QuangLink <100298893+QuangLink@users.noreply.github.com>
2023-10-21 01:53:39 +0700

    add cors origin

--- app.js
+++ app.js
@@ -4,21 +4,15 @@ const createError = require('http-errors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 const app = express();
-
-app.use(function(req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
-  const allowedOrigins = ['http://localhost:3000', ];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-       res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-  next();
-});
-
+app.use(cors({
+  origin: "http://localhost:3000",
+  headers: ["Content-Type"],
+  credentials: true,
+}
+))
+app.options('*', cors())
 // Importing routes
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
