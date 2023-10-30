9a5d827c683f4adddb61e95e72fc9400c83f5615
QuangLink <100298893+QuangLink@users.noreply.github.com>
2023-10-31 03:05:53 +0700

    update comment for product

--- routes/feedback.js
+++ routes/feedback.js
@@ -4,7 +4,9 @@ const db = require("./../models/database");
 
 //get all feedback
 router.get("/", (req, res) => {
-  const query = "SELECT * FROM feedback";
+  const query = `SELECT feedback.*, users.*
+  FROM feedback
+  LEFT JOIN users ON feedback.userID = users.userID`;
   db.query(query, (error, results) => {
     if (error) throw error;
     res.json(results);
@@ -13,7 +15,11 @@ router.get("/", (req, res) => {
 
 //get feedback by prodID
 router.get("/:prodID", (req, res) => {
-  const query = "SELECT * FROM feedback WHERE prodID = ?";
+  const query = `SELECT feedback.*, users.*
+  FROM feedback
+  LEFT JOIN users ON feedback.userID = users.userID
+  WHERE feedback.prodID = ?;
+  `;
   db.query(query, [req.params.prodID], (error, results) => {
     if (error) throw error;
     res.json(results);
@@ -21,17 +27,13 @@ router.get("/:prodID", (req, res) => {
 });
 //post feedback by prodID from client and userID prodRate, comment
 router.post("/", (req, res) => {
-  const {prodID, userID, prodRate, comment } = req.body;
+  const { prodID, userID, prodRate, comment } = req.body;
   const query =
     "INSERT INTO feedback (userID, prodID, prodRate, comment) VALUES (?, ?, ?, ?)";
-  db.query(
-    query,
-    [userID, prodID, prodRate, comment],
-    (error, results) => {
-      if (error) throw error;
-      res.json(results);
-    }
-  );
+  db.query(query, [userID, prodID, prodRate, comment], (error, results) => {
+    if (error) throw error;
+    res.json(results);
+  });
 });
 
 module.exports = router;
