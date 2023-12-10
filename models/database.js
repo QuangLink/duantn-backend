fa026aae1ce18b2334c5cd252da0f3e27aa7c70b
QuangLink <100298893+QuangLink@users.noreply.github.com>
2023-12-10 19:08:41 +0700

    udpate test

--- models/database.js
+++ models/database.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 require('dotenv').config();
 var db = mysql.createConnection({
-   host: "database-1.cvopmbfugv3f.ap-southeast-2.rds.amazonaws.com",
-   user:"jaguarshop",
-   password: "jaguarshop",
-   database: "duantn",
+   host: process.env.HOST,
+   user: process.env.USER, 
+   password: process.env.PASSWORD, 
+   database: process.env.DATABASE,
 }); 
 db.connect(() => console.log('Da ket noi database !'));
 module.exports = db; 
